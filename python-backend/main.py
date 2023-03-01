2aeb044667aec467f854d10c419bcd70edfe6746
Jason R. Stevens, CFA <jason@thinkjrs.dev>
2023-03-01 14:21:48 -0600

    :sparkles: add initial FastAPI example module

---
+++ python-backend/main.py
@@ -0,0 +1,8 @@
+# https://fastapi.tiangolo.com/tutorial/first-steps/
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+async def root_route():
+    return {"message": "Hello, world!"}
